933c5e7c3ff65e1afa643b7075e3d23c2ba43a19
Aronov Alexander <psychosanchez@outlook.com>
2016-05-29 21:25:50 +0300

    Добавлена разметка для профилей и изменения информации. Изменены разметки для табвью. Добавлены методы в ManController.

--- MVCApp/Controllers/MansController.cs
+++ MVCApp/Controllers/MansController.cs
@@ -34,7 +34,7 @@ namespace MVCApp.Controllers
         {
             try
             {
-                IEnumerable<MVCApp.Coachs> coach = db.Coachs.ToList();
+                IEnumerable<MVCApp.Coachs> coach = db.Coachs.Where(x => x.Mans.IsDeleted != true).ToList();
                 return PartialView("_Coach", coach);
             }
             catch
@@ -70,6 +70,39 @@ namespace MVCApp.Controllers
             }
         }
         public PartialViewResult All()
+        {
+            try
+            {
+                IEnumerable<MVCApp.Mans> Mans = db.Mans.Where(x => x.IsDeleted != true).ToList();
+                return PartialView("All", Mans);
+            }
+            catch
+            {
+                return PartialView();
+            }
+        }
+        [HttpPost]
+        public ActionResult All(string searchTerm)
+        {
+            try
+            {
+                IEnumerable<MVCApp.Mans> Mans;
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    Mans = db.Mans.Where(x => x.IsDeleted != true).ToList();
+                }
+                else
+                {
+                    Mans = db.Mans.Where(x => x.FirstName.StartsWith(searchTerm) || x.MiddleName.StartsWith(searchTerm) || x.LastName.StartsWith(searchTerm)).ToList();
+                }
+                return PartialView("All", Mans);
+            }
+            catch
+            {
+                return PartialView();
+            }
+        }
+        public PartialViewResult ShowFired()
         {
             try
             {
@@ -127,18 +160,27 @@ namespace MVCApp.Controllers
         // GET: Mans/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Mans mans = db.Mans.Find(id);
+                if (mans == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.NationalityID = new SelectList(db.Nationaites, "NationalityID", "Nationality", mans.NationalityID);
+                ViewBag.PersonalPositionID = new SelectList(db.PersonalPosition, "PersonalPositionID", "Description", mans.PersonalPositionID);
+                return View(mans);
             }
-            Mans mans = db.Mans.Find(id);
-            if (mans == null)
+            catch (Exception ex)
             {
-                return HttpNotFound();
+                return new HttpStatusCodeResult(HttpStatusCode.Conflict, ex.Message);
             }
-            ViewBag.NationalityID = new SelectList(db.Nationaites, "NationalityID", "Nationality", mans.NationalityID);
-            ViewBag.PersonalPositionID = new SelectList(db.PersonalPosition, "PersonalPositionID", "Description", mans.PersonalPositionID);
-            return View(mans);
         }
 
         // POST: Mans/Edit/5
@@ -160,6 +202,7 @@ namespace MVCApp.Controllers
         }
 
         // GET: Mans/Delete/5
+        [HttpPost]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -167,11 +210,13 @@ namespace MVCApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Mans mans = db.Mans.Find(id);
+            mans.IsDeleted = true;
+            db.SaveChanges();
             if (mans == null)
             {
                 return HttpNotFound();
             }
-            return View(mans);
+            return View("Index");
         }
 
         // POST: Mans/Delete/5
